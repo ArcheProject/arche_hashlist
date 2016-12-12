43076bb9946a3e2d76160e3a6c9065642d6fc587
Robin Harms Oredsson <robin@betahaus.net>
2016-12-12 11:58:26 +0100

    Javascript helper to hash

--- arche_hashlist/views.py
+++ arche_hashlist/views.py
@@ -3,6 +3,7 @@ from arche.views.base import BaseView
 from pyramid.view import view_config
 
 from arche_hashlist.interfaces import IHashList
+from arche_hashlist import _
 
 
 @view_config(context=IHashList,
@@ -12,6 +13,25 @@ class HashListView(BaseView):
 
     def __call__(self):
         return {}
+
+
+@view_config(context=IHashList,
+             name='work_on_hashing.json',
+             renderer='json',
+             permission=PERM_MANAGE_SYSTEM,)
+class HashListWork(BaseView):
+
+    def __call__(self):
+        remaining = self.context.hash_plaintext(limit=100)
+        if remaining:
+            msg = self.request.localizer.translate(
+                _("${num} remaining...", mapping={'num': remaining})
+            )
+        else:
+            msg = self.request.localizer.translate(
+                _("All done")
+            )
+        return {'remaining': remaining, 'msg': msg}
 
 
 def includeme(config):
